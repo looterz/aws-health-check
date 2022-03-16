ed37b3f95ade8e268e949a37b000cebd966c2159
looterz <looter222@gmail.com>
2022-03-16 00:51:34 -0400

    Added more options and reduced the health check response bytes

--- main.go
+++ main.go
@@ -10,8 +10,14 @@ import (
 func main() {
 	log.Println("Starting aws-health-check")
 
-	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		_, err := fmt.Fprintf(w, "healthy\n")
+	addr := flag.String("address", "0.0.0.0", "local network address to listen on")
+	port := flag.Int64("port", 27015, "The port to host the http server on")
+	path := flag.String("path", "/health", "url path to respond to health checks on")
+	flag.Parse()
+
+	http.HandleFunc(fmt.Sprintf("%s", *path), func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte(""))
 		if err != nil {
 			log.Println(err)
 			return
@@ -19,12 +25,9 @@ func main() {
 		log.Printf("successfully responded to health check from %s", r.RemoteAddr)
 	})
 
-	port := flag.Int64("port", 27015, "The port to host the http server on")
-	flag.Parse()
+	log.Printf("Listening for HTTP health checks on endpoint %s:%d%s", *addr, *port, *path)
 
-	log.Printf("Listening for HTTP health checks on port %d", *port)
-
-	var err = http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
+	var err = http.ListenAndServe(fmt.Sprintf("%s:%d", *addr, *port), nil)
 	if err != nil {
 		log.Fatal(err)
 		return
